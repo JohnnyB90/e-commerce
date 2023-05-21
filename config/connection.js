d370af39c7c69c3aeb3f245dd6872a71b91c3517
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-05-21 02:51:35 -0400

    Fixed error with connection.js not being able to locate path to .env file

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,22 @@
-require('dotenv').config();
-
-const Sequelize = require('sequelize');
+require('dotenv').config({ path: '../.env' });
+const Sequelize = require("sequelize");
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+
+      {
+        host: "localhost",
+        dialect: "mysql",
+        port: 3306,
+      }
+    );
+console.log(process.env.DB_NAME);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env);
 
 module.exports = sequelize;
